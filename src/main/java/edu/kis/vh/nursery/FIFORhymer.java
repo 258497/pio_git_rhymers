ad30e883adfadd51177238512090185d7a6cddcd
Anton Sidarovich <258497@edu.p.lodz.pl>
2026-03-23 10:16:46 +0100

    4.1 Poprawa nazw klas

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,19 +1,19 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
-    @Override
-    public int countOut() {
-        while (!callCheck())
-            temp.countIn(super.countOut());
+	@Override
+	public int countOut() {
+		while (!callCheck())
+			temp.countIn(super.countOut());
 
-        int ret = temp.countOut();
+		int ret = temp.countOut();
 
-        while (!temp.callCheck())
-            countIn(temp.countOut());
+		while (!temp.callCheck())
+			countIn(temp.countOut());
 
-        return ret;
-    }
+		return ret;
+	}
 }
